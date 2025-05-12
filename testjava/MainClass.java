619e8e9be894e86d7b0b95066754608b15b57a46
kiyohn0508 <kiyohn0508@gmail.com>
2025-05-12 07:45:28 +0000

    learn 인터페이스, 추상 클래스

--- testjava/MainClass.java
+++ testjava/MainClass.java
@@ -1,11 +1,20 @@
 
 public class MainClass{
     public static void main(String[] args){
-        new ChildClass() {
-            @Override
-            public void method(){
-                System.out.println("methold");
-            };
-        }.method();
+        String str = "JAVA";
+        System.out.println("str :"+str);
+        str = str+ "_8";
+        System.out.println("str :"+str);
+
+        StringBuffer stb = new StringBuffer("JAVA");
+        System.out.println("str :"+stb);
+        stb.append(" world");
+        System.out.println("str :"+stb);
+        System.out.println("str.length :"+stb.length());
+        stb.insert(1, "!!!");
+        System.out.println("str :"+stb);
+
+        stb.delete(1, 3);
+        System.out.println("str :"+stb);
     }
 }
---
+++ testjava/lambdaInterface.java
@@ -0,0 +1,3 @@
+public interface lambdaInterface {
+    public void method(String s1, String s2, String s3);
+}
